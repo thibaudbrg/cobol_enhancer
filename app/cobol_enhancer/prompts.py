7ef71fe4710bc788214952a5ad9ff692961c798c
Thibaud Bourgeois <tibo@Thibauds-MacBook-Pro.local>
2024-04-16 13:32:01 +0200

    Small prompt changes

--- app/cobol_enhancer/prompts.py
+++ app/cobol_enhancer/prompts.py
@@ -4,16 +4,27 @@ from app.cobol_enhancer.utils import get_previous_critic_description
 
 
 def analyze_file_prompt() -> str:
-    prompt = (
-        "You are an expert in code analysis with a focus on COBOL. Examine the original provided code: {filename}. "
-        "Identify any errors, discrepancies or possible enhancement with good usages. Provide a detailed critique, "
-        "highlighting each issue with a thorough explanation and recommended solutions. Your review will guide "
-        "developers in refining the code. This version is under scrutiny for accuracy and adherence to best "
-        "practices. \n\n"
+    prompt = """
+        You are an expert in code analysis with a focus on COBOL. Examine the original provided code: {filename}. 
+        Identify any errors, discrepancies or possible enhancement with good usages. Provide a detailed critique, 
+        highlighting each issue with a thorough explanation and recommended solutions. Your review will guide 
+        developers in refining the code. This version is under scrutiny for accuracy and adherence to best 
+        practices.
 
-        "===========================================\n"
-        "Original COBOL Code:\n{old_code}\n\n"
-    )
+        Among all of your critics, it's crucial to focus on the following aspects:
+        - Check for more comments for a better understanding, not too much tho, just the right amount.
+        - Argue about the termination method. The termination method must not be changed.
+        - Argue that the original line numbers on the left side of each line of code must be preserved. In fact some
+        developers do sign here by replacing the 6 digit number, these line numbers are essential for
+        tracking who wrote the lines purposes. So don't remove or alter these line numbers or pseudos if they are there.
+        
+        It's crucial that overall you don't try to announce changes everywhere, just subtle but meaningful changes.
+        
+        ===========================================
+        Original COBOL Code:
+        
+        {old_code}
+    """
 
     return prompt
 
@@ -74,27 +85,28 @@ def critic_generation_prompt(state: Dict[str, Any]) -> str:
 
 
 def generation_prompt(state: Dict[str, Any]) -> str:
-    prompt_template = """
-    You are an AI with expertise in COBOL, tasked with refining a piece of code. 
-    A new version of {filename} has been generated to improve upon the old code. 
-    Your objective is to correct these mistakes, ensuring the updated code remains 
-    true to its original functionality and improves upon it where possible.
-
-    Based on the critics, refine the generated code to solve the identified issues. 
-    Ensure the final version is optimized, error-free, and faithful to the original's functionality. 
-    Your output should be the corrected code only.
-
-    It's crucial that you don't remove existing comments. Even more important, it is crucial that you add more 
-    comments for a better understanding, not too much tho, just the right amount.
-    The comments must take care of COBOL formatting (73 wide display)
-
-    It's crucial to preserve the original line numbers on the left side of each line of code. Some developers do sign
-    here by replacing the 6 digit number.
-    These line numbers are essential for tracking and documentation purposes. Please make sure that any 
-    modifications you suggest do not remove or alter these line numbers.
-    """
-
     if "original_critic" in state and state["original_critic"]:
+        prompt_template = """
+            You are an AI with expertise in COBOL, tasked with refining a piece of code. 
+            Your objective is to correct these mistakes, ensuring the updated code remains 
+            true to its original functionality and improves upon it where possible.
+
+            Based on the critics, refine the code to solve the identified issues. 
+            Ensure the final version is optimized, error-free, and faithful to the original's functionality. 
+            Your output should be the corrected code only.
+
+            It's crucial that you don't remove existing comments. Even more important, it is crucial that you add more 
+            comments for a better understanding, not too much tho, just the right amount.
+
+            It's crucial to NOT change the termination method of the program. For example, don't introduce a new termination
+            method like "STOP RUN" if it was not there before.
+
+            It's crucial to preserve the original line numbers on the left side of each line of code. Some developers do sign
+            here by replacing the 6 digit number.
+            These line numbers are essential for tracking and documentation purposes. Please make sure that any 
+            modifications you suggest do not remove or alter these line numbers.
+            """
+
         template_extension = """
         Enhance the following COBOL code by refining and optimizing it while maintaining 
         the original functionality. Ensure the final version is error-free:
@@ -105,47 +117,70 @@ def generation_prompt(state: Dict[str, Any]) -> str:
         The critics:
         {original_critic}
         """
-    elif "atlas_message_type" in state and state["atlas_message_type"]:
-        template_extension = """
-        The COBOL code has encountered a {atlas_message_type}. Correct the code 
-        to address the following issue and ensure it is optimized and error-free:
-
-        {atlas_answer}
-
-        Original Code:
-        {old_code}
-
-        Generated Code with errors:
-        {new_code}
-        """
-    elif "specific_demands" in state and state["specific_demands"]:
-        template_extension = """
-        Refine the COBOL code according to the specific demands of the developer 
-        and ensure that all improvements are faithful to the original functionality:
-        
-        Specific demands:
-        {specific_demands}
-
-        Original Code:
-        {old_code}
-
-        Generated Code with errors:
-        {new_code}
-        """
     else:
-        template_extension = """
-        Enhance the following COBOL code by refining and optimizing it while maintaining 
-        the original functionality. Ensure the final version is error-free:
+        prompt_template = """
+            You are an AI with expertise in COBOL, tasked with refining a piece of code. 
+            A new version of {filename} has been generated to improve upon the old code. 
+            Your objective is to correct these mistakes, ensuring the updated code remains 
+            true to its original functionality and improves upon it where possible.
 
-        The critics:
-        {critic}
+            Based on the critics, refine the generated code to solve the identified issues. 
+            Ensure the final version is optimized, error-free, and faithful to the original's functionality. 
+            Your output should be the corrected code only.
 
-        Original Code:
-        {old_code}
+            It's crucial that you don't remove existing comments. Even more important, it is crucial that you add more 
+            comments for a better understanding, not too much tho, just the right amount.
 
-        Generated Code:
-        {new_code}
-        """
+            It's crucial to NOT change the termination method of the program. For example, don't introduce a new termination
+            method like "STOP RUN" if it was not there before.
+
+            It's crucial to preserve the original line numbers on the left side of each line of code. Some developers do sign
+            here by replacing the 6 digit number.
+            These line numbers are essential for tracking and documentation purposes. Please make sure that any 
+            modifications you suggest do not remove or alter these line numbers.
+            """
+
+        if "atlas_message_type" in state and state["atlas_message_type"]:
+            template_extension = """
+            The COBOL code has encountered a {atlas_message_type}. Correct the code 
+            to address the following issue and ensure it is optimized and error-free:
+        
+            {atlas_answer}
+        
+            Original Code:
+            {old_code}
+        
+            Generated Code with errors:
+            {new_code}
+            """
+        elif "specific_demands" in state and state["specific_demands"]:
+            template_extension = """
+            Refine the COBOL code according to the specific demands of the developer 
+            and ensure that all improvements are faithful to the original functionality:
+            
+            Specific demands:
+            {specific_demands}
+        
+            Original Code:
+            {old_code}
+        
+            Generated Code with errors:
+            {new_code}
+            """
+        else:
+            template_extension = """
+            Enhance the following COBOL code by refining and optimizing it while maintaining 
+            the original functionality. Ensure the final version is error-free:
+        
+            The critics:
+            {critic}
+        
+            Original Code:
+            {old_code}
+        
+            Generated Code:
+            {new_code}
+            """
 
     return prompt_template + template_extension
 
